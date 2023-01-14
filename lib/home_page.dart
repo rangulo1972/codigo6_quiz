510756afba2cd7f2cd4e1c3a3dfc7ce4e178c5a6
rangulo1972 <rangulo1972@gmail.com>
2023-01-13 22:17:12 -0500

    Modificando home_page con widget Expanded

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,15 +7,38 @@ class HomePage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
+            flex: 9,
             child: Center(
-              child: Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                textAlign: TextAlign.center,
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 22.0),
+                ),
               ),
             ),
           ),
-          ElevatedButton(onPressed: () {}, child: Text("Hola")),
-          ElevatedButton(onPressed: () {}, child: Text("Hola")),
+          Expanded(
+            child: MaterialButton(
+              onPressed: () {},
+              color: Colors.blue,
+              child: Text(
+                "Verdadero",
+              ),
+            ),
+          ),
+          Expanded(
+            child: MaterialButton(
+              onPressed: () {},
+              color: Colors.red,
+              child: Text(
+                "Falso",
+              ),
+            ),
+          ),
+//          ElevatedButton(onPressed: () {}, child: Text("Hola")),
+//          ElevatedButton(onPressed: () {}, child: Text("Hola")),
         ],
       ),
     );
