bec53c2948af5f47e4a054146a2e86e158e1e818
rangulo1972 <rangulo1972@gmail.com>
2023-01-16 21:21:05 -0500

    creamos una clase del tipo Question para poder instancias las preguntas con sus respectivas respuestas

---
+++ lib/question.dart
@@ -0,0 +1,12 @@
+//creamos la clase del tipo question
+class Question {
+  //Atributos de la clase
+  final String questionText;
+  final bool questionAnswer;
+
+  //constructor de la clase
+  Question({
+    required this.questionText,
+    required this.questionAnswer,
+  });
+}
