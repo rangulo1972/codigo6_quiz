a82252d4f370fff587c822ab412aafbd2de403e3
rangulo1972 <rangulo1972@gmail.com>
2023-01-16 21:00:32 -0500

    poniendo lógica a las respuestas de las preguntas, comparamos las repuestas con las preguntas, actulizamos estado para que indique correcto o falso según a las respuestas escogidas

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,28 +6,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //cramos el puntero de posición para el listado de preguntas
+  int questionNumber = 0;
   //creamos la lista de preguntas
   List<String> questions = [
-    "La tierra es plana.",
-    "El hombre llegó a la luna.",
-    "Los meses del año son 13",
-    "Un año bisiesto es cada 3 años",
-    "Los colores primarios son 4",
+    "La tierra es plana.", //false
+    "El hombre llegó a la luna.", //true
+    "Los meses del año son 13", //false
+    "Un año bisiesto es cada 3 años", //false
+    "Los colores primarios son 4", //false
   ];
+  //creamos listado de respuestas
+  List<bool> answers = [false, true, false, false, false];
 
   //creamos una lista de widgets para mostrar el score de aciertos
-  List<Widget> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Color(0xff3bceac),
-      size: 30.0,
-    ),
-    Icon(
-      Icons.close,
-      color: Color(0xfffe6d73),
-      size: 30.0,
-    ),
-  ];
+  List<Icon> scoreKeeper = [];
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +36,7 @@ class _HomePageState extends State<HomePage> {
               child: Padding(
                 padding: EdgeInsets.all(6.0),
                 child: Text(
-                  questions[1],
+                  questions[questionNumber],
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 22.0, color: Colors.white),
                 ),
@@ -51,18 +44,28 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           Expanded(
+            //botón de verdadero
             //hacemos que el botón tenga margen
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: MaterialButton(
                 onPressed: () {
-                  scoreKeeper.add(
-                    Icon(
+                  //capturamos la respuesta a la pregunta realizada
+                  bool correctAnswer = answers[questionNumber];
+                  //hacemos la comparación de la respuesta correcta a la pregunta
+                  if (correctAnswer == true) {
+                    scoreKeeper.add(Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ));
+                  } else {
+                    scoreKeeper.add(Icon(
                       Icons.close,
-                      color: Colors.white,
-                    ),
-                  );
+                      color: Colors.red,
+                    ));
+                  }
                   setState(() {});
+                  questionNumber++;
                 },
                 color: Colors.green,
                 child: const Text(
@@ -72,10 +75,28 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           Expanded(
+            //botón de respuesta Falso
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: MaterialButton(
-                onPressed: () {},
+                onPressed: () {
+                  //capturamos la respuesta a la pregunta realizada
+                  bool correctAnswer = answers[questionNumber];
+                  //hacemos la comparación de la respuesta correcta a la pregunta
+                  if (correctAnswer == false) {
+                    scoreKeeper.add(Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ));
+                  } else {
+                    scoreKeeper.add(Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ));
+                  }
+                  setState(() {});
+                  questionNumber++;
+                },
                 color: Colors.red,
                 child: const Text(
                   "Falso",
